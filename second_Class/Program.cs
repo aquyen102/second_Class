d790b54c976d4f78c903829e5714605d87e5483d
hv <hv@PM309-19>
2023-03-19 08:44:28 +0700

    Add project files.

---
+++ second_Class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace second_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Xin moi nhap hai so nguyen a;b bat ky");
+            Console.WriteLine("a=");
+            int a= int.Parse(Console.ReadLine());
+            Console.WriteLine("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hai so a;b da nhap la " + a + " ; " + b);
+            Console.ReadKey();
+        }
+    }
+}
