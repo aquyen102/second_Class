f72bd433e67c4cb86a9a12424c469281f4db5481
aquyen102 <aquyen102@gmail.com>
2023-03-19 10:15:16 +0700

    Them ham tinh toan

--- second_Class/Program.cs
+++ second_Class/Program.cs
@@ -15,8 +15,23 @@ namespace second_Class
             int a= int.Parse(Console.ReadLine());
             Console.WriteLine("b=");
             int b = int.Parse(Console.ReadLine());
-            Console.WriteLine("Hai so a;b da nhap la " + a + " ; " + b);
+
+            double c = (double)a / b;
+            Console.WriteLine("Thuong hai so a&b da nhap la {0} ",c);
+            Console.Write("<------------------>\n");
+
+            double d = a * (1.0f) / b;
+            Console.WriteLine("Thuong hai so a&b da nhap la {0} ", d);
+            Console.Write("<------------------>\n");
+
+            Console.WriteLine("Phan du cua phep chia a&b la "+ chia_lay_phan_du(a, b));
+
             Console.ReadKey();
+        }
+        static double chia_lay_phan_du(int a, int b)
+        {
+            double c = a % b;
+            return c;
         }
     }
 }
