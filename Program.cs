4d51890e8425934d0bdc7e952875330879b7249b
aquyen102 <128267787+aquyen102@users.noreply.github.com>
2023-03-23 16:26:34 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_tap_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nhap so lieu
+            Console.WriteLine("Moi ban nhap chieu dai canh a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Moi ban nhap chieu dai canh b");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("<-------------KET QUA--------------->");
+
+            //Tinh toan va xuat du lieu
+            Console.WriteLine("Dien tich cua Hinh Chu Nhat a & b la {0}", a * b);
+            Console.WriteLine("Chu vi cua Hinh Chu Nhat a & b la {0}", 2*(a+b));
+            Console.ReadKey();
+        }
+    }
+}
